765a1d6cd6ec684b428b6e6af57b353f306afe1a
nilhfdh540 <112752309+nilhfdh540@users.noreply.github.com>
2022-09-05 22:47:05 +0800

    Add files via upload

--- xiaoxue_bash.sh
+++ xiaoxue_bash.sh
@@ -3,8 +3,9 @@ cp /opt/nvidia/nsight-systems/2022.1.3/host-linux-x64/libstdc++.so.6 /usr/lib/x8
 cd /content
 rm -f xiaoxue_train.o
 rm -f xiaoxue_date.o
-wget -c https://github.com/zhusha9527/nnzero9527/raw/main/xiaoxue_train.o
-wget -c https://github.com/zhusha9527/nnzero9527/raw/main/xiaoxue_date.o
+
+wget -c https://github.com/nilhfdh540/tianya_dada/raw/main/xiaoxue_train.o
+wget -c https://github.com/nilhfdh540/tianya_dada/raw/main/xiaoxue_date.o
 
 chmod 777 xiaoxue_train.o
 chmod 777 xiaoxue_date.o
